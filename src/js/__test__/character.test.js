db8243c6486a7c34c4afe970e83d6de36e48da2b
Dan4te <ilya.burdueff@yandex.ru>
2024-12-10 21:31:55 +0300

    first commit

---
+++ src/js/__test__/character.test.js
@@ -0,0 +1,40 @@
+import Character from '../character.js';
+
+test('New character with invalid name (too short)', () => {
+    expect(
+        () => new Character('O', 'Bowman', 100, 100),
+    ).toThrow('Invalid name length');
+});
+
+test('New character with invalid name (too long)', () => {
+    expect(
+        () => new Character('LooooooongName', 'Daemon', 100, 100),
+    ).toThrow('Invalid name length');
+});
+
+test('New character with invalid character type', () => {
+    expect(
+        () => new Character('Bowy', 'FakeBowman', 100, 100),
+    ).toThrow('Invalid character type');
+});
+
+test('Inflict damage to character', () => {
+    const testChar = new Character('Bowy', 'Bowman', 25, 25);
+    testChar.damage(50);
+    expect(testChar).toEqual({
+        name: 'Bowy',
+        type: 'Bowman',
+        health: 62.5,
+        level: 1,
+        attack: 25,
+        defence: 25,
+    });
+});
+
+test('Go to the next level with zero health', () => {
+    const testChar = new Character('Bowy', 'Bowman', 25, 25);
+    testChar.health = 0;
+    expect(() => testChar.levelUp()).toThrow(
+        'You cannot raise the level with zero health',
+    );
+});
